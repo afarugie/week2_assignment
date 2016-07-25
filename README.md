1bf36d08679e42640ee6411a9e6bd5ca1995df40
Arrio Farugie <arrio.farugie@gmail.com>
2016-07-25 10:11:59 -0500

    clean up menu.rb

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ TODO: further test all files.  Recreate the menu with TDD. Remove logic
 from menu.rb ( converting cents to dollars )
 ISSUES: If a user presses enter without selecting an item, 
 the first item (index 0) will be added to the cart. 
-
-Questions: Is it proper to specify 'self.' when calling an instance method
-from inside another instance method?" (example https://github.com/afarugie/week2_assignment/blob/master/lib/menu.rb#L20 )
--- lib/menu.rb
+++ lib/menu.rb
@@ -6,18 +6,16 @@ class Menu
 
   def initialize 
     @cart = Cart.new
-    arduino_lawn_mower = Item.new("Arduino Lawn Mower", 159.37)
-    electric_guitar = Item.new("Electric Guitar", 2500.79)
-    lasagna = Item.new("Lasagna", 47.99)
-    red_stapler = Item.new("Red Stapler", 25.99)
-    window_shutters = Item.new("Window Shutters", 85.00)
-    @items = [lasagna, red_stapler, electric_guitar, arduino_lawn_mower,
-              window_shutters]
+    @items = [Item.new("Arduino Lawn Mower", 159.37),
+              Item.new("Electric Guitar", 2500.79),
+              Item.new("Lasagna", 47.99),
+              Item.new("Red Stapler", 25.99),
+              Item.new("Window Shutters", 85.00) ]
   end
 
   def add_to_cart(choice)
     @cart.add_item(@items[choice])
-    self.keep_shopping?
+    keep_shopping?
   end
 
   def banner
@@ -43,17 +41,17 @@ class Menu
 
   def keep_shopping?
     puts "would you like to continue shopping? y/n: "
-    self.shop self.get_user_input
+    shop get_user_input
   end
 
   def shop(choice)
-    if self.cart.items.count == 0
-      self.add_to_cart choice.to_i
+    if cart.items.count == 0
+      add_to_cart choice.to_i
     elsif choice == 'n'
-      self.checkout
+      checkout
     elsif choice.to_i.is_a? Numeric
-      self.banner
-      self.add_to_cart self.get_user_input.to_i
+      banner
+      add_to_cart get_user_input.to_i
     end
   end
 
