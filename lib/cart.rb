8bcadef1a700fc2439d7f0dc4cb789bbc8dc06d3
Brian Hogan <bphogan@gmail.com>
2016-08-14 13:55:31 -0500

    Update cart.rb

--- lib/cart.rb
+++ lib/cart.rb
@@ -7,20 +7,18 @@ class Cart
   end
 
   def add_item(item)
-    unless item.is_a? Item
-      raise Exception, "#{item} is not a valid Item"
-    else
-      @items[item] += 1
-    end
+    raise Exception, "#{item} is not a valid item" unless item.is_a? Item
+    @items[item] += 1
   end
 
   def total
-    total = BigDecimal.new(0)
-    items.each do |key, value|
-      cost = key.price * value
-      total += cost
-    end
-    total.to_dollars
+    # see Enum.reduce for hashes.  
+    #   acc is the accumulator, then a tuple of the key and val
+    #   Each iterationm current element is key and value
+    #   so we can total up.
+    #   If you have a data structure you need to roll up to a single value, reach for reduce.
+    @items.reduce(0){|acc, (key, val)| acc + (key.price * val)}
+         .to_dollars
   end
 
   def items
